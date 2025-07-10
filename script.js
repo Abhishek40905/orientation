98c07b9542a20e1ff6203dbbb8a98881f43d887a
abhishek <abhiy40905@gmail.com>
2025-07-10 13:07:21 +0530

    initial files added

---
+++ script.js
@@ -0,0 +1,27 @@
+function sendData() {
+  const cupon_no = document.getElementById('cuponInput').value.trim();
+  const params = Object.fromEntries(new URLSearchParams(window.location.search));
+  const user_id = params.enrollment_no;
+  if (!cupon_no) {
+    responseMsg.style.color = 'red';
+    responseMsg.textContent = "Please enter a coupon number.";
+    console.log(user_id);
+    return;
+  }
+  fetch('http://localhost:3000/input-data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user_id, cupon_no })
+  }).then((responseMsg)=>{
+     if(responseMsg.status==200){
+        alert('user entered successfully');
+     }
+     if(responseMsg.status==201){
+        alert('duplicate entry');
+     }
+  }).catch((err)=>{
+    console.log(err);
+  })
+}
